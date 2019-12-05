aeba46f6102bb3ca73c19d58a6ceaa799d18f62b
Óscar Carrasco <oxcabe@gmail.com>
2019-12-05 12:11:27 +0000

    Umpersand

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ uint16_t get_potenciometro() {
 	uint8_t poten[4];
 	uint16_t ret = 0;
 
-	if (atd_devuelveValores(port, poten, 4) == 1) {
+	if (atd_devuelveValores(port, &poten, 4) == 1) {
 		// Convert to uint16_t
 		for (uint8_t i = 0; i < 4; i++) {
 			ret += poten[i] << i;
