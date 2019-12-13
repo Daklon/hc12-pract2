f916aff29d86152179fe076746124578edaf3f73
Óscar Carrasco <oxcabe@gmail.com>
2019-12-13 22:42:58 +0000

    Fix merge conflicts

--- src/main.c
+++ src/main.c
@@ -34,22 +34,37 @@
 #define FILA_DOS 0
 #define FILA_TRES 1
 #define FILA_CUATRO 3
-
 #define NINGUNA_TECLA_PULSADA 4
 
+/** Selects which digit from the number array is going to 
+ * be displayed in the seven-segment. */
 uint8_t display_digit = 0;
+/** Contains the digit to display in the seven-segment. */
 uint8_t number[4];
+/** Represents the numeric keyboard. */
 char matrix_teclado[3][4];
+/** Represents the timeout state for the 
+ * teclado_getch_timeout function. */
 uint8_t boolean_timeout = 0;
 
+/** Port where the display is connected to. */
 char display_port = 'G';
+/** Port where the numeric keyboard is connected to. */
 char teclado_port = 'H';
 
+/** Initialises the seven-segment module configuring G
+ *  port as an output interface. */
 void sieteSeg_init(){
     //incializamos el puerto de salida G
     e_s_total(display_port,255);
 }
 
+/** Displays a 4-byte numeric array integer values in the seven-segment.
+ *
+ * It is important that the array passed to this function as an argument is at least 4 bytes wide.
+ *
+ * @param value Pointer to the 8-bit integer array.
+*/
 void sieteSeg_digitos(uint8_t* value){
     uint8_t i;
     uint8_t mask = (1 << 7);//10000000
@@ -60,6 +75,10 @@ void sieteSeg_digitos(uint8_t* value){
     }
 }
 
+/** Displays a value in the seven-segment.
+ *
+ * @param value 16-bit unsigned integer value to display.
+*/
 void sieteSeg_valor(uint16_t value){
     int8_t i = 3;
     uint8_t mask = (1 << 4);
@@ -72,6 +91,7 @@ void sieteSeg_valor(uint16_t value){
     }
 }
 
+/** Updates the values displayed in the seven-segment module. */
 void update_siete_seg(){
     if(display_digit > 3){
         display_digit = 0;
@@ -80,6 +100,7 @@ void update_siete_seg(){
     display_digit++;
 }
 
+/** Initialises the potentiometer. */
 void potenciometro_init() {
     if (
         atd_setBits(0, 10) &&
@@ -97,6 +118,11 @@ void potenciometro_init() {
         }
 }
 
+/** Retrieves a 16-bit unsigned integer value from the potentiometer output port. 
+ *
+ * @return 16-bit, unsigned integer value that represents the potentiometer position.
+ *
+*/
 uint16_t get_potenciometro() {
 	uint8_t port = 0;
 	uint16_t poten[4];
@@ -113,6 +139,11 @@ uint16_t get_potenciometro() {
 	return ret;
 }
 
+/** Permutes the column values based on the received input.
+ *
+ * @param pin Value that multiplexes the control flow. To
+ * make effect, it has to be between 0 and 3.
+*/
 void set_teclado_scan_out(uint8_t pin){
     switch (pin){
         case 0:
@@ -138,6 +169,7 @@ void set_teclado_scan_out(uint8_t pin){
     }
 }
 
+/** Initialises the numeric keyboard. */
 void teclado_init(){
     //configuramos puerto T como s,s,e,s,e,s,e,e
     e_s_total(teclado_port,212);
@@ -166,6 +198,10 @@ void teclado_init(){
     matrix_teclado[2][3] = '#';
 }
 
+/** Gets which row is being pressed at the time.
+ *
+ * @return Unsigned integer value representing that row.
+*/
 uint8_t get_teclado_inputs(){
     if (leer_pin(teclado_port,FILA_UNO) == 0){
         return 0;
@@ -179,6 +215,10 @@ uint8_t get_teclado_inputs(){
     return NINGUNA_TECLA_PULSADA; //sin tecla pulsada
 }
 
+/** Gets which key is being pressed at the time.
+ *
+ * @return char with the key value.
+*/
 char teclado_getch(){
     uint8_t column,row;
     //comprobamos si ya hay una tecla pulsada, si es así esperamos a que se suelte
@@ -202,10 +242,18 @@ char teclado_getch(){
     return matrix_teclado[row][column];
 }
 
+/** Sets the boolean timeout for the teclado_getch_timeout function. */
 void timeout(){
     boolean_timeout = 1;
 }
 
+/** Gets which key is being pressed at the time. If it takes too long, it
+ * times out, sending the character 'T'.
+ *
+ * @param milis 32-bit unsigned integer that contains the milliseconds before timeout.
+ *
+ * @return char with the key value, or 'T' if it times out.
+*/
 char teclado_getch_timeout(uint32_t milis){
     future_f(&timeout,milis*1000);
     uint8_t column,row; 
@@ -236,6 +284,14 @@ char teclado_getch_timeout(uint32_t milis){
     return matrix_teclado [row][column];
 }
 
+/** Converts a number stored as an numeric array into a 8-bit unsigned integer.
+ *
+ * Example: [1, 2, 3] --> 123
+ *
+ * @param value Pointer to array to be processed.
+ *
+ * @return 8-bit unsigned integer obtained from processing the array.
+*/
 uint8_t array_to_uint(uint8_t* value){
     uint8_t temp = 0;
     uint8_t multiplier = 1;
@@ -246,6 +302,14 @@ uint8_t array_to_uint(uint8_t* value){
     return temp;
 }
 
+/** Right-shifts 4-byte array elements and assigns a new value to
+ * the less significant element.
+ *
+ * @param value Pointer to array to be processed.
+ *
+ * @param new_value char value to set in the less significant element.
+ *
+*/
 void set_shifted_value(uint8_t* value,char new_value){
     for(uint8_t i = 3;i>0;i--){
         value[i] = value[i-1];
@@ -253,20 +317,38 @@ void set_shifted_value(uint8_t* value,char new_value){
     value[0] = new_value- '0';
 }
 
+/** Initialises the motor or fan. */
 void motor_init(){
     pwmBase(0, 0, 0, 1, 100, 15, 0);
 }
 
+/** Establishes the motor speed through the pwm interface. 
+ *
+ * @value speed 8-bit unsigned integer to assign as the new speed value.
+ *
+*/ 
 void set_motor_speed(uint8_t speed){
     cambiaPWDTYporciento(speed);
 }
 
+/** Copies a 4-byte array.
+ *
+ * @value to Pointer to copy array.
+ * @value from Pointer to array to be copied.
+ *
+*/ 
 void copyto(uint8_t* to, uint8_t* from){
     for (uint8_t i = 0;i < 4;i++){
        to[i] = from[i]; 
     }
 }
 
+/** Main program loop.
+ *
+ * Reads a number from keyboard-based input and displays it in a seven-segment array, causing a small fan to rotate with a speed value proportional to the keyboard input.
+ *
+ * @return Returns an error code (more than 0) if the program does not exit gracefully. 
+*/
 int main(){
     uint16_t i = 0;
     uint8_t keyboard_input[4],temp[4];
@@ -315,4 +397,5 @@ int main(){
 		sieteSeg_valor(array_to_uint(temp));//imprimimos temp
     }
 }
+
  /*@}*/
